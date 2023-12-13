db4ae27f93e5fb9685f03c2f9e4cb334c1eac9f8
Eis <80625199+EisTheEgg@users.noreply.github.com>
2023-12-13 10:56:02 +0100

    Day 1

---
+++ Day1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() 
+{
+    const string numbers = "1234567890";
+    int sum = 0;
+
+    string line;
+
+    ifstream file("Day1Input.txt");
+
+    if (file.is_open()) {
+        while(getline(file, line)) {
+            size_t first = line.find_first_of(numbers);
+            size_t last = line.find_last_of(numbers);
+
+            string combination = string(1, line[first]) + string(1, line[last]);
+            
+            sum += stoi(combination);
+        }
+
+        file.close();
+    }
+
+    cout<<sum<<endl;
+
+    return 0;
+}
